47867f9a58b5022036e1e99a5d1dff4be80992d6
Andrew Zakharkin <zakharking@gmail.com>
2015-12-26 23:15:40 +0300

    init

---
+++ src/aspnet5/Controllers/HelloController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using System.Reflection;
+using MiscClasses;
+
+namespace aspnet5.Controllers
+{
+    [Route("api/[controller]"), Route("/")]
+    public class HelloController:Controller
+    {
+        [Route("[action]"), Route("")]
+        public string Index2()
+        {
+            var greeter = new Greeter();
+            return greeter.GetGreeting()+
+#if DNX451
+
+             Assembly.GetExecutingAssembly().FullName;
+#endif
+#if DNXCORE50
+             typeof(HelloController).AssemblyQualifiedName+"!!!"; 
+#endif
+        }
+    }
+}
