92f7a0cea3850ba35163596d4921645380a8af06
Andrew Zakharkin <zakharking@gmail.com>
2016-05-03 22:44:07 +0300

    Angular 2

---
+++ src/SocketServer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocketServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from DNX!");
+        }
+
+    }
+}
