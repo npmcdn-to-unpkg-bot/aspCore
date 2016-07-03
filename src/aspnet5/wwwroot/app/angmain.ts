f2ba5f27552252e11212530e5c74c15095fba41b
Andrew Zakharkin <zakharking@gmail.com>
2016-07-03 22:02:03 +0300

    Update

---
+++ src/aspnet5/wwwroot/app/angmain.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by andre on 03.07.2016.
+ */
+import {bootstrap} from "node_modules/angular2/platform/browser.js";
+import {Component} from "node_modules/angular2/core.js";
+@Component(
+    selector: 'app',
+    template: '<div>Hello Andrew!</div>'
+)
+class App{
+
+}
+bootstrap(App);
--- src/aspnet5/wwwroot/config.js
+++ src/aspnet5/wwwroot/config.js
@@ -4,10 +4,24 @@ System.config({
     typescriptOptions: {
         "emitDecoratorMetadata": true
     },
+
+    map: {
+        'rxjs': 'node_modules/rxjs/Rx.js',
+        '@angular/core' :
+            'node_modules/angular2/core.js',
+        '@angular/common' :
+            'https://npmcdn.com/@angular/common@2.0.0-rc.1',
+        '@angular/compiler' :
+            'https://npmcdn.com/@angular/compiler@2.0.0-rc.1',
+        '@angular/platform-browser' :
+            'https://npmcdn.com/@angular/platform-browser@2.0.0-rc.1',
+        '@angular/platform-browser-dynamic':
+            'https://npmcdn.com/@angular/platform-browser-dynamic@2.0.0-rc.1'
+    }
     packages: {
         "app": {
-            "defaultExtension": "ts",
-            "main": "main.ts"
+            "defaultExtension": "js",
+            "main": "angmain.ts"
         }
     }
 });
